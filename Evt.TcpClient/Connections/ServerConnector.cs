31d6c9e8e5ac33a69d2a6bb8b832ca41e944f2fb
jqd072014@yahoo.com <jqd072014@yahoo.com>
2017-07-24 00:20:23 -0400

    QDS-1: add start retry

--- Evt.TcpClient/Connections/ServerConnector.cs
+++ Evt.TcpClient/Connections/ServerConnector.cs
@@ -13,6 +13,8 @@ namespace Evt.Communication.Connections
 {
     public class ServerConnector
     {
+        const int CONN_CHK_INTERVAL_SEC = 10;
+        const int START_RETRY_INTERVAL_SEC = 10;
         Dictionary<ServerName, TcpClient> _serverClients = new Dictionary<ServerName, TcpClient>();
 
         public void Start()
@@ -21,63 +23,87 @@ namespace Evt.Communication.Connections
             {
                 ServerName serverName;
                 Enum.TryParse<ServerName>(n.ToString(), out serverName);
-                var client = BuildServerClientByName(serverName);
-                if (client != null)
-                    _serverClients.Add(serverName, client);
+                TryBuildServerClientByName(serverName);
             }
             SetupCheckAndRestoreConnections();
         }
 
+        private void TryBuildServerClientByName(ServerName serverName)
+        {
+            var client = BuildServerClientByName(serverName);
+            if (client != null)
+            {
+                _serverClients.Add(serverName, client);
+                return;
+            }
 
-        const int POLL_INTERVAL_SEC = 10;
-        const int POLL_DURATION_MS = 100;
+            Observable.Interval(TimeSpan.FromSeconds(START_RETRY_INTERVAL_SEC)).TakeWhile(s=>client==null).Subscribe(s =>
+            {
+                client = BuildServerClientByName(serverName);
+                if (client != null)
+                {
+                    _serverClients.Add(serverName, client);
+                }
+            });
+        }
+
         void SetupCheckAndRestoreConnections()
         {
-            Observable.Interval(TimeSpan.FromSeconds(POLL_INTERVAL_SEC)).Subscribe(s =>
+            Observable.Interval(TimeSpan.FromSeconds(CONN_CHK_INTERVAL_SEC)).Subscribe(s =>
             {
-                foreach (var sc in _serverClients)
+                for (int i=0; i<_serverClients.Count;i++)
                 {
-                    bool isConnected = IsConnected(sc.Value.Client, sc.Key);
-                    bool isReadable = IsReadable(sc.Value.Client);
-                    bool isWritable = sc.Value.Client.Poll(POLL_DURATION_MS, SelectMode.SelectWrite);
-                    bool hasError = sc.Value.Client.Poll(POLL_DURATION_MS, SelectMode.SelectWrite);
-                    Debug.WriteLine("{0} {1}", sc.Key, isConnected);
-                    if (!isConnected || !isReadable || !isWritable || hasError)
+                    try
                     {
-                        //Debug.WriteLine("{0} {1} {2} {3} {4} ", sc.Key,
-                        //    isConnected ? "Connected" : "Disconnected",
-                        //    isReadable?"readable":"notReadable", "", "");
-                        ////isWritable?"writable":"notWritable",
-                        ////hasError?"noError":"hasError");
+                        var sc = _serverClients.ElementAt(i);
+                        bool isConnected = sc.Value!=null && IsConnected(sc.Value.Client, sc.Key);
+
+                        LOG(string.Format("{0} {1}", sc.Key, isConnected));
+                        if (!isConnected)
+                        {
+                            LOG(string.Format("try reconnecting {0}...", sc.Key));
+                            if (sc.Value != null)
+                              sc.Value.Client.Shutdown(SocketShutdown.Both); 
+
+                            _serverClients[sc.Key] = BuildServerClientByName(sc.Key);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        LOG(string.Format("{0} {1}", ex.Message, ex.StackTrace));
                     }
                 }
             });
-
-
-
         }
 
         private bool IsConnected(Socket sock, ServerName serverName)
         {
-            if(!sock.Connected)
+            try
             {
-                Debug.WriteLine("{0} Desktop Tcp caller has a problem and fail to connect", serverName);
+                if (!sock.Connected)
+                {
+                    LOG(string.Format("{0} Desktop Tcp caller has a problem and fail to connect", serverName));
+                    return false;
+                }
+
+                if (!IsPhysicalLevelConnected(sock))
+                {
+                    LOG(string.Format("{0} ping failed, could be Physical Server rebooting.", serverName));
+                    return false;
+                }
+
+                if (!IsApplicationLevelConnected(sock))
+                {
+                    var ep = sock.RemoteEndPoint as IPEndPoint;
+                    LOG(string.Format("{0} not responding from {1}:{2}, it could be restarting.", serverName, ep.Address, ep.Port));
+                    return false;
+                }
+            }
+            catch(Exception ex)
+            {
+                LOG(string.Format("{0} {1}", ex.Message, ex.StackTrace));
                 return false;
             }
-
-            if (!IsPhysicalLevelConnected(sock))
-            {
-                Debug.WriteLine("{0} ping failed, could be Physical Server rebooting.", serverName);
-                return false;
-            }
-
-            if(!IsApplicationLevelConnected(sock))
-            {
-                var ep = sock.RemoteEndPoint as IPEndPoint;
-                Debug.WriteLine("{0} not responding from {1}:{2}, it could be restarting.", serverName,ep.Address,ep.Port  );
-                return false;
-            }
-
             return true;
         }
 
@@ -117,23 +143,22 @@ namespace Evt.Communication.Connections
             }
         }
 
-        bool IsReadable(Socket sock)
-        {
-            if (!sock.Connected) return false;
-            bool readable =sock.Poll(POLL_DURATION_MS, SelectMode.SelectRead);
-            bool hasDataToRead = sock.Available>0;
-          //  Debug.WriteLine("readable {0} hasDataToRead {1}", readable, hasDataToRead);
-            return readable && hasDataToRead;
-        }
-
         private TcpClient BuildServerClientByName(ServerName serverName)
         {
-            var t = new TcpClient();
-            var hostNameOrIP = _serverConfigs[serverName].HostName;
-            var port = _serverConfigs[serverName].Port;
-            t.Connect(hostNameOrIP,port);
- 
-            return t;
+            try
+            {
+                var t = new TcpClient();
+                var hostNameOrIP = _serverConfigs[serverName].HostName;
+                var port = _serverConfigs[serverName].Port;
+                t.Connect(hostNameOrIP, port);
+                return t;
+            }
+            catch(Exception ex)
+            {
+                LOG(string.Format("{0} {1}", ex.Message, ex.StackTrace));
+            }
+
+            return null;
         }
 
         Dictionary<ServerName, ServerConfig> _serverConfigs = new Dictionary<ServerName, ServerConfig>()
@@ -144,11 +169,40 @@ namespace Evt.Communication.Connections
             //{ ServerName.ComplianceServer, new ServerConfig() {HostName="eqpwk-emm13",Port=31019 } },
             //{ ServerName.FuseServer, new ServerConfig() {HostName="eqpwk-emm13",Port=31013} }
 
-            { ServerName.OrderRoutingServer, new ServerConfig() {HostName="192.168.0.101",Port=31017 } },
-            { ServerName.VegaHitterServer, new ServerConfig() {HostName="192.168.0.101",Port=31224 } },
-            { ServerName.EvtVolPublisher, new ServerConfig() {HostName="192.168.0.101",Port=25249 } },
-            { ServerName.ComplianceServer, new ServerConfig() {HostName="192.168.0.101",Port=31019 } },
-            { ServerName.FuseServer, new ServerConfig() {HostName="192.168.0.101",Port=31013} }
+            { ServerName.OrderRoutingServer, new ServerConfig() {HostName="192.168.0.104",Port=31017 } },
+            { ServerName.VegaHitterServer, new ServerConfig() {HostName="192.168.0.104",Port=31224 } },
+            { ServerName.EvtVolPublisher, new ServerConfig() {HostName="192.168.0.104",Port=25249 } },
+            { ServerName.ComplianceServer, new ServerConfig() {HostName="192.168.0.104",Port=31019 } },
+            { ServerName.FuseServer, new ServerConfig() {HostName="192.168.0.104",Port=31013} }
         };
+
+        static void LOG(string msg)
+        {
+            Debug.WriteLine(msg);
+            Console.WriteLine(msg);
+        }
     }
 }
+
+
+//const int POLL_DURATION_MS = 100;
+//bool IsReadable(Socket sock)
+//{
+//    if (!sock.Connected) return false;
+//    bool readable = sock.Poll(POLL_DURATION_MS, SelectMode.SelectRead);
+//    bool hasDataToRead = sock.Available > 0;
+//    //  Debug.WriteLine("readable {0} hasDataToRead {1}", readable, hasDataToRead);
+//    return readable && hasDataToRead;
+//} 
+
+//bool isReadable = IsReadable(sc.Value.Client);
+//bool isWritable = sc.Value.Client.Poll(POLL_DURATION_MS, SelectMode.SelectWrite);
+//bool hasError = sc.Value.Client.Poll(POLL_DURATION_MS, SelectMode.SelectWrite);
+//            if (!isConnected || !isReadable || !isWritable || hasError)
+//            {
+//            Debug.WriteLine("{0} {1} {2} {3} {4} ", sc.Key,
+//                isConnected ? "Connected" : "Disconnected",
+//                isReadable ? "readable" : "notReadable", "", "");
+//            isWritable ? "writable" : "notWritable",
+//                hasError ? "noError" : "hasError");
+//}

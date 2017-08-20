f17252b4bb426dbcbef52eb64cf6761b74d1fd49
jqd072014@yahoo.com <jqd072014@yahoo.com>
2017-08-20 15:34:50 -0400

    QDS-10: .py in pycharm Quantlib Instrument and pricing Engine

--- quantlib Notebooks/pycharm/Instrument and pricing Engine.py
+++ quantlib Notebooks/pycharm/Instrument and pricing Engine.py
@@ -19,3 +19,52 @@ engine=AnalyticEuropeanEngine(process)
 option.setPricingEngine(engine)
 
 print('{0:.4f} {1:.4f} {2:.4f} {3:.4f} {4:.4f}'.format(option.NPV(),option.delta(), option.vega(),option.gamma(),option.theta()))
+
+u.setValue(95)
+print('{0:.4f} {1:.4f} {2:.4f} {3:.4f} {4:.4f}'.format(option.NPV(),option.delta(), option.vega(),option.gamma(),option.theta()))
+
+import numpy
+import matplotlib.pyplot as plot
+from IPython.display import display
+
+
+plot.interactive(True)
+f, ax=plot.subplots()
+xs= numpy.linspace(80.0,120.0,400)
+ys=[]
+for x in xs:
+    u.setValue(x);
+    ys.append(option.NPV())
+ax.set_title("option Value")
+_=ax.plot(xs,ys)
+
+#plot.show(block=True)
+
+Settings.instance().evaluationDate=Date(14,September,2017)
+
+ys=[]
+for x in xs:
+    u.setValue(x)
+    ys.append(option.NPV())
+ax.plot(xs,ys)
+display(f)  # f=fig object
+
+plot.show(block=True)
+
+# dummy starting params
+v0=0.04; kappa=0.1;theta=0.01;sigma=0.05; rho=-0.75
+model =HestonModel(
+    HestonProcess(YieldTermStructureHandle(rfCurve), YieldTermStructureHandle(FlatForward(0,TARGET(),0.0,Actual360())),
+                  QuoteHandle(u), v0,kappa,theta,sigma,rho))
+
+engine=AnalyticHestonEngine(model)
+option.setPricingEngine(engine)
+
+print(option.NPV())
+
+engineMC=MCEuropeanEngine(process,"PseudoRandom", timeSteps=20, requiredSamples=250000)
+option.setPricingEngine(engineMC)
+
+Settings.instance().evaluationDate=today
+
+print('{0:.4f} '.format(option.NPV())) # MC cannot est greek, must use Fininte Difference
